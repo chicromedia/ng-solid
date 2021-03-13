14ee3c649b6f29098046d1c3a422db143c19084c
chicromedia <chicromedia@hotmail.com>
2021-03-13 10:29:49 -0400

    Implementación de twitter fix dependencias

--- packages/twitter/src/twitter.module.ts
+++ packages/twitter/src/twitter.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { NsTwitterComponent } from './components/share/share.component';
 import { CommonModule } from "@angular/common";
+import { NsCoreModule } from "@ng-solid/core";
+import { NsTwitterService } from "./services/twitter.service";
 
 @NgModule({
-  imports: [ CommonModule ],
-  declarations: [ NsTwitterComponent ],
-  exports: [ NsTwitterComponent ]
+  imports: [
+    CommonModule,
+    NsCoreModule
+  ],
+  declarations: [
+    NsTwitterComponent
+  ],
+  providers: [
+    NsTwitterService
+  ],
+  exports: [
+    NsTwitterComponent
+  ]
 })
 export class NsTwitterModule
 {
