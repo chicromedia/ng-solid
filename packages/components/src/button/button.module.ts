80044ee3fdf9310a5797fd9dadad51a955e0e2d4
chicromedia <chicromedia@hotmail.com>
2022-08-12 10:29:50 -0400

    AP-8 : Implementación del Dashboard : Fix disabled and loading ns-button

--- packages/components/src/button/button.module.ts
+++ packages/components/src/button/button.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NsButtonGroupComponent } from './components/button-group/button-group.component';
 import { NsButtonComponent } from "./components/button/button.component";
+import { NsIconModule } from '../icon/icon.module';
 
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        NsIconModule
+    ],
   declarations: [
     NsButtonComponent,
     NsButtonGroupComponent
--- packages/components/src/button/components/button/button.component.ts
+++ packages/components/src/button/components/button/button.component.ts
@@ -5,7 +5,9 @@ import { Component, Input, OnInit } from '@angular/core';
     templateUrl: './button.component.html',
     styleUrls: [ './button.component.scss' ],
     host: {
-        '[class.ns-button__grid]': 'isBlock'
+        '[class.ns-button__grid]': 'isBlock',
+        '[class.ns-button--loading]': 'loading',
+        '[class.ns-button--disabled]': 'disabled'
     }
 } )
 export class NsButtonComponent implements OnInit
@@ -20,6 +22,10 @@ export class NsButtonComponent implements OnInit
     href: string;
     @Input()
     isBlock: boolean;
+    @Input()
+    disabled: boolean;
+    @Input()
+    loading: boolean;
 
     constructor()
     {
