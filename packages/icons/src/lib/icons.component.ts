fc261590b201038b9aee4a6355607f181da50e6a
Ulises Familia Rodriguez <chicromedia@hotmail.com>
2023-12-29 14:22:19 -0400

    chore: forzar deploy (#37) (#38)

--- packages/icons/src/lib/icons.component.ts
+++ packages/icons/src/lib/icons.component.ts
@@ -33,7 +33,6 @@ export class IconsComponent implements OnChanges
     constructor( private renderer: Renderer2,
                  private service: NsIconsService)
     {
-
     }
 
     ngOnChanges( { name }: SimpleChanges )
